9d49816fc810f817e94c5948895093a410bc9242
Ram-Amoncar <ramamonkar444@gmail.com>
2025-04-14 17:34:25 +0530

    chore(lint): migrate to eslint's flag config

---
+++ eslint.config.js
@@ -0,0 +1,47 @@
+'use strict'
+
+import stylistic from '@stylistic/eslint-plugin'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import jest from 'eslint-plugin-jest'
+import love from 'eslint-config-love'
+
+export default [
+    {
+        ignores: ['dist', '**/*.js']
+    },
+    // Stylistic
+    {
+        plugins: {
+            '@stylistic': stylistic
+        },
+        rules: {
+            '@stylistic/indent': ['error', 4],
+            '@stylistic/quotes': ['error', 'single', { avoidEscape: true, allowTemplateLiterals: true }],
+            '@stylistic/space-before-function-paren': 'off',
+            '@stylistic/no-unmodified-loop-condition': 'off'
+        }
+    },
+    // eslint-config-love
+    {
+        ...love
+    },
+    // Jest
+    {
+        files: ['test/**/*.test.ts', 'tests/**/*.test.ts', 'test/**/*.spec.ts', 'tests/**/*.spec.ts'],
+        ...jest.configs['flat/recommended'],
+        rules: {
+            ...jest.configs['flat/recommended'].rules,
+            ...jest.configs['flat/style'].rules
+        }
+    },
+    // Prettier
+    eslintPluginPrettierRecommended,
+    // Custom
+    {
+        rules: {
+            '@typescript-eslint/no-magic-numbers': 'off',
+            'jest/consistent-test-it': ['error', { fn: 'test' }]
+        },
+        files: ['tests/**.test.ts']
+    }
+]
